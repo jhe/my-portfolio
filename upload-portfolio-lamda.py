3cbe603e395050c779ecc1033d3e1a2dcb61706a
Jeff Rozic <jjrozic@gmail.com>
2017-09-14 15:20:30 -0700

    updated lambda function

--- upload-portfolio-lamda.py
+++ upload-portfolio-lamda.py
@@ -8,13 +8,20 @@ def lambda_handler(event, context):
     bb = s3.Bucket('portfolio-build.jeffrozic.com')
     pb = s3.Bucket('portfolio.jeffrozic.com')
     zf = io.BytesIO()
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-west-1:899238936541:deployPortfolioTopic')
     
-    bb.download_fileobj('portfolioBuild.zip', zf)
-    
-    with zipfile.ZipFile(zf) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            pb.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            pb.Object(nm).Acl().put(ACL='public-read')
+    try:
+        bb.download_fileobj('portfolioBuild.zip', zf)
+        with zipfile.ZipFile(zf) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                pb.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                pb.Object(nm).Acl().put(ACL='public-read')
+        
+        topic.publish(Subject='Portfolio Deployed', Message='My Portfolio was successfully deployed')
+    except:
+        topic.publish(Subject='Portfolio deploy failed', Message='My Portfolio was not deployed')        
+        raise
     
     print('Job done!')
